c028cb26065fee32bf8671c11a05d7556ee1dd36
Admin <Admin@DESKTOP-BN3UKJ6>
2017-10-01 00:38:06 +0300

    added ItemTest

---
+++ Test.Unit/ItemTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Crawler.Logic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Unit
+{
+    [TestClass]
+    public class ItemTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestParentOnlyOne()
+        {
+            var parent1 = new Item("", "");
+            var parent2 = new Item("", "");
+            var child = new Item("", "");
+
+            parent1.AddItem(child);
+            parent2.AddItem(child);
+        }
+
+        [TestMethod]
+        public void TestGetRoot()
+        {
+            var parent1 = new Item("", "");
+            var parent2 = new Item("", "");
+            var parent3 = new Item("", "");
+
+            parent1.AddItem(parent2);
+            parent2.AddItem(parent3);
+
+            Assert.AreEqual(parent3.GetRoot(), parent1);
+        }
+    }
+}
